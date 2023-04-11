feeb3e7c86e7898e895b0cd3f655df0082b160f3
dralexnasirian <110799675+dralexnasirian@users.noreply.github.com>
2023-04-11 14:18:44 +1000

    Create CH3_6.R

---
+++ Wickham_book_apps/CH3_6.R
@@ -0,0 +1,39 @@
+
+library(shiny)
+
+# Define UI for application that draws a histogram
+ui <- fluidPage(
+    fluidRow(
+        column(3,
+               numericInput("lambda1","lambda1",value = 3),
+               numericInput("lambda2","lambda2",value = 5),
+               numericInput("n","n",value = 1e3,min = 0),
+               actionButton("simulate","Simulate!")
+               ),
+        column(9,
+               plotOutput("hist")
+               )
+    )
+)
+
+# Define server logic required to draw a histogram
+server <- function(input, output, session) {
+    
+    x1 <- reactive({
+        input$simulate
+        rpois(input$n, input$lambda1)
+    })
+    
+    x2 <- reactive({
+        input$simulate
+        rpois(input$n, input$lambda2)
+    })
+    
+    output$hist <- renderPlot({
+        freqpoly(x1(), x2(), binwidth = 1,xlim = c(0,40))
+    })
+       
+}
+
+# Run the application 
+shinyApp(ui = ui, server = server)
